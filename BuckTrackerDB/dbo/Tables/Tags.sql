c46c6cf40e0d6f8ffa5b71ddc89632bf0aac17da
varlackc <varlackc@mail.gvsu.edu>
2019-10-08 23:26:19 -0500

    Tags table configuration
    Tags Table Configuration

--- BuckTrackerDB/dbo/Tables/Tags.sql
+++ BuckTrackerDB/dbo/Tables/Tags.sql
@@ -1,4 +1,7 @@
 ﻿CREATE TABLE [dbo].[Tags]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [TagDescription] NVARCHAR(50) NULL, 
+    [TagType] NCHAR(10) NULL, 
+    [TimeStamp] DATETIME2 NULL
 )
