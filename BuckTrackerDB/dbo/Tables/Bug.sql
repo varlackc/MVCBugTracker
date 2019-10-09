7f54e0614501985b38e4da8f523d7695a4d5168b
varlackc <varlackc@mail.gvsu.edu>
2019-10-08 23:24:37 -0500

    Bug Table Configuration
    Bug Table Configuration

--- BuckTrackerDB/dbo/Tables/Bug.sql
+++ BuckTrackerDB/dbo/Tables/Bug.sql
@@ -1,4 +1,12 @@
 ﻿CREATE TABLE [dbo].[Bug]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ProjectId] INT NULL, 
+    [Description] NVARCHAR(50) NULL, 
+    [TimeStamp] NVARCHAR(50) NULL, 
+    [Status] NCHAR(10) NULL, 
+    [CreatedByUserId] INT NULL, 
+    [Details] NVARCHAR(50) NULL, 
+    [PriorityLevel] NCHAR(10) NULL, 
+    [UserAssignedId] INT NULL
 )
