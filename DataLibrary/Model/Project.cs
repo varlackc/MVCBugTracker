fc73c9d93986264f7f20ddf5ae33459241df03aa
varlackc <varlackc@mail.gvsu.edu>
2019-10-15 22:55:01 -0500

    Update project class
    update project class

--- DataLibrary/Model/Project.cs
+++ DataLibrary/Model/Project.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace DataLibrary.Model
 {
-    class Project
+    public class Project
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime DeadLine { get; set; }
+        public int BugId { get; set; }
     }
 }
