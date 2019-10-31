cf045b2f57ea13ba0d614889565977d3ea4a62ac
varlackc <varlackc@mail.gvsu.edu>
2019-10-31 18:26:00 -0500

    Correct bugs
    correct bugs

--- MVCBuckTrackerDemo/Controllers/BugController.cs
+++ MVCBuckTrackerDemo/Controllers/BugController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using DataLibrary;
 using static DataLibrary.BusinessLogic.BugProcessor;
+
 using System.Web.Mvc;
 
 namespace MVCBugTrackerDemo.Controllers
@@ -12,32 +13,31 @@ namespace MVCBugTrackerDemo.Controllers
     public class BugController : Controller
     {
         //------------ Bug Section ------------------
-
+        
         //Regular Project View
         public ActionResult BugList(int id)
         {
-            //load the data
-            var data = LoadBugs(id);
-            //create a list of projects
-            List<BugModel> projects = new List<BugModel>();
+                //load the data
+                var data = LoadBugs(id);
+                //create a list of projects
+                List<BugModel> bugs = new List<BugModel>();
 
-            // loop to organize the data in the projects list
-            foreach (var row in data)
-            {
-                projects.Add(new BugModel
+                // loop to organize the data in the projects list
+                foreach (var row in data)
                 {
-                    Id = row.Id,
-                    Description = row.Description,
-                    Status = row.Status,
-                    Details = row.Details,
-                    PriorityLevel = row.PriorityLevel,
-                    BugProjectId = row.BugProjectId
-
-                });
-            }
+                    bugs.Add(new BugModel
+                    {
+                        Id = row.Id,
+                        Description = row.Description,
+                        Status = row.Status,
+                        Details = row.Details,
+                        PriorityLevel = row.PriorityLevel,
+                        BugProjectId = row.BugProjectId
+                    });
+                }
 
             ViewBag.message = id;
-            return View(projects);
+            return View(bugs);
         }
 
 
