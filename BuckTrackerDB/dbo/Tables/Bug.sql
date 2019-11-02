6cdf034de9eebaff48083f53a55cd909328d2d1d
varlackc <varlackc@mail.gvsu.edu>
2019-11-02 12:13:01 -0500

    Improve the application
    Improve the application

--- BuckTrackerDB/dbo/Tables/Bug.sql
+++ BuckTrackerDB/dbo/Tables/Bug.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [dbo].[Bug]
 (
 	[Id] INT NOT NULL PRIMARY KEY  IDENTITY(1 ,1),
-    [Description] NVARCHAR(50) NULL, 
-    [TimeStamp] NVARCHAR(50) NULL, 
+    [Description] NVARCHAR(100) NULL, 
+    [TimeStamp] DATETIME2 NULL, 
     [Status] NCHAR(10) NULL, 
     [Details] NVARCHAR(50) NULL, 
     [PriorityLevel] NCHAR(10) NULL, 
