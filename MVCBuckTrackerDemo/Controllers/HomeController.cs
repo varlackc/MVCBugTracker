3d58f6cdde4df7a8ee1733b36211399dc733fe4f
varlackc <varlackc@mail.gvsu.edu>
2019-11-07 17:25:25 -0600

    Eliminate empty spaces
    Eliminate empty spaces in the home controller

--- MVCBuckTrackerDemo/Controllers/HomeController.cs
+++ MVCBuckTrackerDemo/Controllers/HomeController.cs
@@ -21,14 +21,12 @@ namespace MVCBugTrackerDemo.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
